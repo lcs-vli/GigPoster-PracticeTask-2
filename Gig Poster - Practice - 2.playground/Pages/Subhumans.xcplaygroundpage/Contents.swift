e75057ccc195e06fae010031fc081d486e07282b
Victoria Li <VLi@lcs.on.ca>
2020-03-05 15:07:25 -0500

    4 brown shapes with 2 for in loops

--- Gig Poster - Practice - 2.playground/Pages/Subhumans.xcplaygroundpage/Contents.swift
+++ Gig Poster - Practice - 2.playground/Pages/Subhumans.xcplaygroundpage/Contents.swift
@@ -32,9 +32,34 @@ let purple = Color(hue: 304, saturation: 40, brightness: 64, alpha: 100)
 let brown = Color(hue: 38, saturation: 99, brightness: 38, alpha: 100)
 let lightRed = Color(hue: 10, saturation: 80, brightness: 80, alpha: 75)
 
-// Begin your solution here...
+//set background colour
+canvas.fillColor = yellow
+canvas.drawRectangle(at: Point(x:0, y:0), width: 400, height: 600)
 
 
+
+//brown and purple
+for shuShape in 0...1{
+    
+    for hengShape in 0...1{
+        var paralleclVertices: [Point] = []
+        canvas.drawShapesWithFill = true
+        canvas.drawShapesWithBorders = false
+        canvas.fillColor = brown
+        //bottom left
+        paralleclVertices.append(Point(x: -140 + hengShape*320, y: 50+shuShape*175))
+        //botom right
+        paralleclVertices.append(Point(x: 10 + hengShape*320, y: 50+shuShape*175))
+        //top right
+        paralleclVertices.append(Point(x: 60 + hengShape*320, y: 150+shuShape*175))
+        //top left
+        paralleclVertices.append(Point(x: -90 + hengShape*320, y: 150+shuShape*175))
+
+        canvas.drawCustomShape(with: paralleclVertices)
+        
+    }
+    
+}
 /*:
  ## Use Source Control
  
